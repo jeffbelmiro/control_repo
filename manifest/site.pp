94ff502534a5255af191e76309e7a56753bc17c8
jeffbelmiro <jeffbelmiro@gmail.com>
2022-06-20 10:02:35 -0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,11 +1,10 @@
 node default {
-file {'/root/README':
-  ensure  => file,
-  content => 'This is a Readme',
-  owner   => 'root',
+ file {'/root/README':
+   ensure  => file,
+   content => 'This is a Readme',
+   owner   => 'root',
   }
-file {'/root/README':
-  owner => 'root',
-    }
-  
- }
+ file {'/root/README':
+   owner => 'root',
+  }
+}
